f828363214cd1610dd7ceb4d21175b1a771b9f86
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-03-01 17:21:41 +0000

    Add SSL and change the default database_url

--- NevaManagement.Api/Startup.cs
+++ NevaManagement.Api/Startup.cs
@@ -71,13 +71,14 @@ namespace NevaManagement.Api
 
         public string GetConnectionString()
         {
-            var uriString = Configuration.GetConnectionString("DefaultConnectionString");
+            var uriString = Environment.GetEnvironmentVariable("DATABASE_URL")
+                                ?? Configuration.GetConnectionString("DATABASE_URL");
             var uri = new Uri(uriString);
             var db = uri.AbsolutePath.Trim('/');
             var user = uri.UserInfo.Split(':')[0];
             var passwd = uri.UserInfo.Split(':')[1];
             var port = uri.Port > 0 ? uri.Port : 5432;
-            var connStr = string.Format("Server={0};Database={1};User Id={2};Password={3};Port={4}",
+            var connStr = string.Format("Server={0};Database={1};User Id={2};Password={3};Port={4};SSL Mode=Require;Trust Server Certificate=True;",
                 uri.Host, db, user, passwd, port);
             return connStr;
         }
