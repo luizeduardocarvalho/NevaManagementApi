21a927e9d4d24d1f0375aec9cdb5e7138aa53f64
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-04-15 19:16:47 +0100

    Create organism related DTOs

---
+++ MevaManagement.Domain/Dtos/Organism/EditOrganismDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NevaManagement.Domain.Dtos.Organism
+{
+    public class EditOrganismDto
+    {
+        [Required]
+        public long Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
