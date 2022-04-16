114c8a6952b680dd6186c2dc533b22aa9d291ec0
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-04-16 02:05:02 +0100

    Create GetSimpleResearcher DTO

---
+++ MevaManagement.Domain/Dtos/Researcher/GetSimpleResearcherDto.cs
@@ -0,0 +1,8 @@
+﻿namespace NevaManagement.Domain.Dtos.Researcher
+{
+    public class GetSimpleResearcherDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
