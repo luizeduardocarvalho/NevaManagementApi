b021f95159e1f19b4759aa9a7065b1bd46f4680a
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-06-01 18:52:48 +0100

    Add missing package reference

--- NevaManagement.Api/GlobalUsings.cs
+++ NevaManagement.Api/GlobalUsings.cs
@@ -7,6 +7,7 @@ global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Options;
 global using Microsoft.IdentityModel.Tokens;
 global using Microsoft.OpenApi.Models;
 global using NevaManagement.Api.Configurations;
