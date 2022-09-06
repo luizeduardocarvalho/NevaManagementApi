6c354d03676d9e3d91ec1e91bdc61f6ed038f58e
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-09-06 23:47:11 +0100

    Add logger and error validation for the TokenService

--- NevaManagement.Api/Configurations/TokenService.cs
+++ NevaManagement.Api/Configurations/TokenService.cs
@@ -1,37 +1,49 @@
-﻿namespace NevaManagement.Api.Configurations;
+﻿using Microsoft.Extensions.Logging;
+
+namespace NevaManagement.Api.Configurations;
 
 public class TokenService : ITokenService
 {
     private readonly IOptions<Settings> settings;
+    private readonly ILogger<TokenService> logger;
 
-    public TokenService(IOptions<Settings> settings)
+    public TokenService(IOptions<Settings> settings, ILogger<TokenService> logger)
     {
         this.settings = settings;
+        this.logger = logger;
     }
 
     public async Task<string> GenerateToken(GetDetailedResearcherDto user)
     {
-        var secret = this.settings.Value.Secret;
-        if (string.IsNullOrEmpty(secret))
-            secret = Environment.GetEnvironmentVariable("Settings");
-
-        var tokenHandler = new JwtSecurityTokenHandler();
-        var key = Encoding.ASCII.GetBytes(secret);
-        var tokenDescriptor = new SecurityTokenDescriptor
+        try
         {
-            Subject = new ClaimsIdentity(new[]
+            var secret = this.settings.Value.Secret;
+            if (string.IsNullOrEmpty(secret))
+                secret = Environment.GetEnvironmentVariable("Settings");
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
             {
-                    new Claim(ClaimTypes.Name, user.Email),
-                    new Claim(ClaimTypes.Role, user.Role)
-                }),
-            Expires = DateTime.UtcNow.AddDays(30),
-            SigningCredentials = new SigningCredentials(
-                new SymmetricSecurityKey(key),
-                SecurityAlgorithms.HmacSha256Signature)
-        };
+                Subject = new ClaimsIdentity(new[]
+                {
+                        new Claim(ClaimTypes.Name, user.Email),
+                        new Claim(ClaimTypes.Role, user.Role)
+                    }),
+                Expires = DateTime.UtcNow.AddDays(30),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
 
-        var token = tokenHandler.CreateToken(tokenDescriptor);
+            var token = tokenHandler.CreateToken(tokenDescriptor);
 
-        return await Task.FromResult(tokenHandler.WriteToken(token));
+            return await Task.FromResult(tokenHandler.WriteToken(token));
+        }
+        catch(Exception e)
+        {
+            logger.LogError(e, e.Message);
+            throw;
+        }
     }
 }
