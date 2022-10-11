428f47d4bb12d0abcf31bf16f340ad5e22a849b7
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-10-11 22:33:00 +0100

    Add authorization

--- NevaManagement.Api/Startup.cs
+++ NevaManagement.Api/Startup.cs
@@ -115,7 +115,7 @@ public class Startup
 
         app.UseEndpoints(endpoints =>
         {
-            endpoints.MapControllers(); //.RequireAuthorization();
+            endpoints.MapControllers().RequireAuthorization();
         });
     }
 
